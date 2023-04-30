613e01b54fc876fd7102a5b4d1ce5e70567d38c1
CodeJuzi <d1741530592@163.com>
2023-04-30 16:29:36 +0800

    update: shell scripts

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git pull origin master
 
 git add .
 git commit -m "$1"
-git ls-files | while read file; do touch -d "$(git log -1 --format="@%ct" "$file" | xargs -I{} date -d @{} +%Y%m%d%H%M.%S)" "$file"; done
+git ls-files | while read file; do touch -d "$(git log -1 --format="@%ct" "$file")" "$file"; done
 
 git push origin master
