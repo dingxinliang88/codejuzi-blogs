69a97c59f5f34a2f37c3fe53ed3e64517848dd0b
CodeJuzi <d1741530592@163.com>
2023-05-01 18:47:06 +0800

    update: bug fix date display error

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
-
 git add .
 git commit -m "$1"
 
-git pull origin master
+git ls-files -z | while IFS=$'\0' read -r -d '' file; do
+  timestamp=$(git log -1 --format=%ct -- "$file")
+  date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
+  touch -t "$date" "$file"
+done
 
 git push origin master
--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
-# git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
+# git ls-files | while read file; do touch -d "$(git log -1 --format='%ad' --date=format-local:%Y%m%d%H%M.%S "$file")" "$file"; done
+git ls-files -z | while IFS=$'\0' read -r -d '' file; do
+  timestamp=$(git log -1 --format=%ct -- "$file")
+  date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
+  touch -t "$date" "$file"
+done
+
 docsify serve;
