7052ebecc9d4da47ab9b433dd069ce9fa6a22790
CodeJuzi <d1741530592@163.com>
2023-05-02 16:18:12 +0800

    update: depoly shell script

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,32 @@
 
 while getopts "t:" opt; do
   case ${opt} in
-    t )
-      message="$OPTARG"
-      touch_files=true
-      ;;
-    \? )
-      echo "Usage: deploy.sh [-t commit_message]" 1>&2
-      exit 1
-      ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" 1>&2
-      exit 1
-      ;;
+  t)
+    message="$OPTARG"
+    touch_files=true
+    ;;
+  \?)
+    echo "Usage: deploy.sh [-t commit_message]" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Invalid option: $OPTARG requires an argument" 1>&2
+    exit 1
+    ;;
   esac
 done
 
 git add .
 if [ -n "$1" ]; then
   git commit -m "$1"
-fi
-
-if [ "$touch_files" = true ]; then
-  git ls-files -z | while IFS=$'\0' read -r -d '' file; do
-    timestamp=$(git log -1 --format=%ct -- "$file")
-    date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
-    touch -t "$date" "$file"
-  done
-
+  if [ "$touch_files" = true ]; then
+    git ls-files -z | while IFS=$'\0' read -r -d '' file; do
+      timestamp=$(git log -1 --format=%ct -- "$file")
+      date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
+      touch -t "$date" "$file"
+    done
+  fi
   git push origin master
 fi
+
+
