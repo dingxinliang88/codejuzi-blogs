89a7bd700abc46764e38d1e69c82327be1f8992b
CodeJuzi <d1741530592@163.com>
2023-05-03 21:27:56 +0800

    update: some configs

--- public/js/docsify-core.js
+++ public/js/docsify-core.js
@@ -19,25 +19,25 @@ window.$docsify = {
   mergeNavbar: true,
   // 封面支持，默认加载的是项目根目录下的_coverpage.md文件
   coverpage: true,
-  // onlyCover: true,
   // 最大支持渲染的标题层级
   maxLevel: 5,
   // 自定义侧边栏后默认不会再生成目录，设置生成目录的最大层级（建议配置为2-4）
   subMaxLevel: 4,
   themeable: {
-    readyTransition: true, // default
+    readyTransition: true, 
   },
   themeable: {
-    readyTransition: true, // default
-    responsiveTables: true, // default
+    readyTransition: true, 
+    responsiveTables: true, 
   },
+  topMargin: 90,
   /* tabs */
   tabs: {
-    persist    : true,      // default
-    sync       : true,      // default
-    theme      : 'classic', // default
-    tabComments: true,      // default
-    tabHeadings: true       // default
+    persist: true, 
+    sync: true, 
+    theme: "classic", 
+    tabComments: true, 
+    tabHeadings: true, 
   },
   /*搜索相关设置*/
   search: {
@@ -68,7 +68,7 @@ window.$docsify = {
     margin: "10px",
     float: "right",
     fontsize: "0.9em",
-    color: "rgb(90,90,90)",
+    color: "RGB(255, 255, 0)",
     language: "chinese",
     isExpected: true,
   },
--- test.sh
+++ test.sh
@@ -1,8 +1,30 @@
-# git ls-files | while read file; do touch -d "$(git log -1 --format='%ad' --date=format-local:%Y%m%d%H%M.%S "$file")" "$file"; done
-git ls-files -z | while IFS=$'\0' read -r -d '' file; do
-  timestamp=$(git log -1 --format=%ct -- "$file")
-  date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
-  touch -t "$date" "$file"
+# !/bin/bash
+
+while getopts "t:" opt; do
+  case ${opt} in
+  t)
+    message="$OPTARG"
+    touch_files=true
+    ;;
+  \?)
+    echo "Usage: deploy.sh [-t commit_message]" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Invalid option: $OPTARG requires an argument" 1>&2
+    exit 1
+    ;;
+  esac
 done
 
-docsify serve;
+if [ -n "$1" ]; then
+  if [ "$touch_files" = true ]; then
+    git ls-files -z | while IFS=$'\0' read -r -d '' file; do
+      timestamp=$(git log -1 --format=%ct -- "$file")
+      date=$(date -r "$timestamp" "+%Y%m%d%H%M.%S")
+      touch -t "$date" "$file"
+    done
+  fi
+fi
+
+docsify serve
